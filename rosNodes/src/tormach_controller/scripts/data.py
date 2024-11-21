d4a8d4099a89b35f8f1a12c1ab7120ca7de308c1
yahboom <yahboom@ros1.myguest.virtualbox.org>
2024-11-21 17:00:38 -0500

    data

---
+++ rosNodes/src/tormach_controller/scripts/data.py
@@ -0,0 +1,41 @@
+import rospy 
+from tormach_controller.msg import pose, forceTorque, MovePoseAction, MovePoseGoal,MovePoseResult,MovePoseFeedback
+from sensor_msgs.msg import JointState
+#need to do pip install read char before running
+from readchar import readchar
+
+pose=[0,0,0,0,0,0]
+effort=[0,0,0,0,0,0]
+
+
+def jointStateCallback(msg):
+    #print("h")
+    #rospy.loginfo(msg.effort);
+    #print(np.array(msg.position)[0:6])
+    #writer.writerow(msg.effort)
+    pose=msg.position[0:6]
+    effort=msg.effort
+
+
+
+if __name__=='__main__':
+    #start the buffer_node node
+    rospy.init_node("buffer_node")    
+    
+    f=open('data.csv','w')
+    
+    #with open('data.csv','wb') as file:
+    #	writer=csv.writer(csvfile)
+    currentPoseSub=rospy.Subscriber("/joint_states", JointState,queue_size=1, callback=jointStateCallback)
+    #keep node running until shutdown request
+    while not  rospy.is_shutdown():
+        c=readchar()
+        f.write(str(pose)+','+str(effort)+'\n')
+        if c=='e':
+            break
+    f.close()
+            
+    #csvfile.close()
+
+
+    
