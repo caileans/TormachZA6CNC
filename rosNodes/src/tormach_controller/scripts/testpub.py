53e0026b69727ab6d87602f0ab426e0c90e1d3cf
Nikolas <nikolashamilton25@gmail.com>
2025-01-07 19:46:59 -0500

    testpub

---
+++ rosNodes/src/tormach_controller/scripts/testpub.py
@@ -0,0 +1,25 @@
+import rospy 
+from trajectory_msgs import JointTrajectory, JointTrajectoryPoint
+    
+
+if __name__=='__main__':
+    
+    #start the test node
+    rospy.init_node("testpub")
+
+    forcePub=rospy.Publisher('/position_trajectory_controller/command', JointTrajectoryPoint)
+
+    pnt=JointTrajectoryPoint()
+    pnt.positions=[0,.22,-.17,.63,.3,.97,25.88,-9.25]
+    pnt.effort=[];
+    pnt.velocities=[];
+    pnt.accelerations=[];
+
+    pubmsg=JointTrajectory();
+    pubmsg.points=[pnt];
+    pubmsg.joint_names=['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6','tcp_lin','tcp_rot']
+
+    forcePub.publish(pubmsg)
+    #keep node running until shutdown request
+    while not  rospy.is_shutdown():
+        x=1;
