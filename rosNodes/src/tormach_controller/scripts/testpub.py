18ffe281dafe0f2e716dd0bbc38123cca20faa09
Nikolas <nikolashamilton25@gmail.com>
2025-01-22 21:21:11 -0500

    asas

--- rosNodes/src/tormach_controller/scripts/testpub.py
+++ rosNodes/src/tormach_controller/scripts/testpub.py
@@ -74,7 +74,7 @@ def movepath(hz):
     pnt.velocities=[]
     # pnt.velocities=[1,0,0,0,0,0,0,0]
     pnt.accelerations=[]
-    pnt.time_from_start.secs=1/hz
+    pnt.time_from_start.nsecs=int((10**9)/hz)
     # rospy.loginfo(pnt)
     pubmsg=JointTrajectory()
     pubmsg.joint_names=['joint_1','joint_2','joint_3','joint_4','joint_5','joint_6','tcp_lin','tcp_rot']
