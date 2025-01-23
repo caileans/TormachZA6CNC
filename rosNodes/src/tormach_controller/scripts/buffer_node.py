2be10e515e3df0fc108b387bf59dfb7b1004ff93
Nikolas <nikolashamilton25@gmail.com>
2025-01-23 18:45:20 -0500

    hahjd

--- rosNodes/src/tormach_controller/scripts/buffer_node.py
+++ rosNodes/src/tormach_controller/scripts/buffer_node.py
@@ -8,9 +8,9 @@ from math import sin, cos, pi
 import csv
 from scipy.optimize import least_squares
 
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-print(sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+# import os, sys
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# print(sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 # import general_robotics_toolbox
 from general_robotics_toolbox import * 
 import TrajectoryPlanner as tp
