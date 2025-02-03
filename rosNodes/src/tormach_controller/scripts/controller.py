c0acaaa53cd085e54b166f42a754e4e9bf6d9bba
Nikolas <nikolashamilton25@gmail.com>
2025-02-03 17:49:55 -0500

    sjsjds

--- rosNodes/src/tormach_controller/scripts/controller.py
+++ rosNodes/src/tormach_controller/scripts/controller.py
@@ -1,8 +1,8 @@
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__)+"/scripts/lib")))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__).split("controller.py")[0]+"/scripts/lib")))
 # print(os.path.dirname(os.path.dirname('/scripts/lib')))
-print(os.path.abspath(__file__.split("controller.py")[0]+"/lib"))
+# print(os.path.abspath(__file__.split("controller.py")[0]+"/lib"))
 print(sys.path)
 import rospy 
 from time import sleep
