a5f51aaf0bdf3bc5548a8094f3a52456d23959fa
Nikolas <nikolashamilton25@gmail.com>
2025-02-03 18:58:30 -0500

    sjsjds

--- rosNodes/src/tormach_controller/scripts/controller.py
+++ rosNodes/src/tormach_controller/scripts/controller.py
@@ -30,7 +30,7 @@ if __name__=='__main__':
     rospy.init_node("controller")
     # x=DataTypes.TrajPoint()
     file ='/home/pathpilot/Downloads/TormachZA6CNC/Gcode/circleTest.nc'
-    # publisher=pub.startPublisher()
+    publisher=pub.startPublisher()
     overshoot=2.0
     robot=ik.tormachZA6()
 
