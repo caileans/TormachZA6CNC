cb47140ba6509e0faa7f43e930fafe451a581424
Nikolas <nikolashamilton25@gmail.com>
2025-02-03 18:20:50 -0500

    sjsjds

--- rosNodes/src/tormach_controller/setup.py
+++ rosNodes/src/tormach_controller/setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
      packages=['preProcessing'],
-     package_dir={'': '../../../preProcessing'}
+     package_dir={'': '../../..'}
 )
 
 setup(**setup_args)
