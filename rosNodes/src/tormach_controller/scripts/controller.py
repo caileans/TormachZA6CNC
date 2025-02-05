8daa6f15644f1f6b23d48f2727f71e030ef3ae27
Nikolas <nikolashamilton25@gmail.com>
2025-02-04 20:21:36 -0500

    ik

--- rosNodes/src/tormach_controller/scripts/controller.py
+++ rosNodes/src/tormach_controller/scripts/controller.py
@@ -59,8 +59,8 @@ if __name__=='__main__':
             # print(point)
             jcur=ik.runIK(np.append(np.array(point.pos[0:3]),point.rot[0:3], axis=0),jprev,robot)
             # print(jcur)
-            jpub=pub.applyOvershoot(jprev,jcur,overshoot)
-            # jpub=jcur
+            # jpub=pub.applyOvershoot(jprev,jcur,overshoot)
+            jpub=jcur
             print(jpub)
             pub.pubMove(publisher,jpub,overshoot,hz)
             jprev=jcur;
