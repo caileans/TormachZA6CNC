80de7d45e3b24cd2720052e12105ca49c2a5244c
Nikolas <nikolashamilton25@gmail.com>
2025-02-13 18:57:19 -0500

    j62rpy

--- rosNodes/src/tormach_controller/scripts/lib/InverseKinematics.py
+++ rosNodes/src/tormach_controller/scripts/lib/InverseKinematics.py
@@ -125,6 +125,8 @@ def R2rpy(R):
     print(np.linalg.norm(R[0:2,0]))
     print(R[0:2,0])
     abc=grtb.R2rpy(R)
+    print(abc)
+    assert np.linalg.norm(R[0:2,0]) > np.finfo(float).eps * 10.0, "Singular rpy requested"
     return np.array([abc[-1],abc[-2],abc[-3]])
 
 def j62rpy(j6):
