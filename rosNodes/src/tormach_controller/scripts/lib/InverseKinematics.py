4533c9ad05b36cb8a43934a2f763565116ecf4fd
Nikolas <nikolashamilton25@gmail.com>
2025-02-19 15:27:08 -0500

    jsdk

--- rosNodes/src/tormach_controller/scripts/lib/InverseKinematics.py
+++ rosNodes/src/tormach_controller/scripts/lib/InverseKinematics.py
@@ -53,9 +53,9 @@ def chooseIK(r0, sols, w):
         # print(np.shape(newsol)[0])
 
         for j in range(np.shape(newsol)[0]):
-            if (j==3 or j==5) and newsol[4]**2<.0004:
-                print(newsol)
-                w[7+j]=100
+            # if (j==3 or j==5) and newsol[4]**2<.0004:
+                # print(newsol)
+                # w[7+j]=100
             newvalue+=w[j]*(newsol[j]-r0[j])**2+w[7+j]*newsol[j]**2
         # print(newvalue)
         # newvalue=(newsol[0]-r0[0])**2+(newsol[5]-r0[5])**2
