1c1218bd34f08fa1486974cffdf1ae0291d6f0f4
Nikolas <nikolashamilton25@gmail.com>
2025-02-18 19:34:32 -0500

    thej

--- rosNodes/src/tormach_controller/scripts/controller.py
+++ rosNodes/src/tormach_controller/scripts/controller.py
@@ -59,7 +59,7 @@ if __name__=='__main__':
         # hz=50
         moveBuffer=Queue(maxsize=0)
 
-        pointList=gct.genTrajectory(file, a=30,hz=hz,feedRate=30,rapidFeed=30,toolFrameOffset=offset)
+        pointList=gct.genTrajectory(file, a=30,hz=hz,feedRate=30,rapidFeed=30,toolFrameOffset=offset,pureRotVel=np.pi/20)
         for point in pointList:
             moveBuffer.put(point)
 
