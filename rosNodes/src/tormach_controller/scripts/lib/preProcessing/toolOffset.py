6a92bdd1da609962814830ed4a598fd5097f80c1
Cailean Sorce <caileansorce@gmail.com>
2025-04-07 22:23:10 -0400

    asdf

--- rosNodes/src/tormach_controller/scripts/lib/preProcessing/toolOffset.py
+++ rosNodes/src/tormach_controller/scripts/lib/preProcessing/toolOffset.py
@@ -49,11 +49,13 @@ def toolOffset(points, toolOffset, nFadeIn = 0, nFadeOut = 0):
 	for i in range(len(points)):
 		if i < nFadeIn:
 			points[i]=applyOffset(points[i],toolOffset*(i*1.0/nFadeIn))
+			points[i].toolVec[1] += .176*(i*1.0/nFadeIn)
 		elif i > (nPoints - nFadeOut):
 			points[i]=applyOffset(points[i],toolOffset*(1-(i-(nPoints-nFadeOut*1.0))/nFadeOut))
+			points[i].toolVec[1] += .176*(1-(i-(nPoints-nFadeOut*1.0))/nFadeOut)
 		else:
 			points[i]=applyOffset(points[i],toolOffset)
-		points[i].toolVec[1] = .176
+			points[i].toolVec[1] += .176
 	return points
 
 # pointList=gct.genTrajectory(__file__.split("TormachZA6CNC")[0]+'TormachZA6CNC/Gcode/5DOFTest.nc', a=30,hz=.5,feedRate=30,rapidFeed=30,toolFrameOffset=np.array([500,0,500]),pureRotVel=np.pi/5)
